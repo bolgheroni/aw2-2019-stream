b3be63c325f04a02efc6f22dfb633431f9a055d3
bolgheroniroberto <30711378+bolgheroni@users.noreply.github.com>
2019-06-04 21:36:51 -0300

    Separando caminho para armazenar as tabelas models

--- back/UsuariosDAO.php
+++ back/UsuariosDAO.php
@@ -1,53 +1,70 @@
 <?php
-$GLOBALS['caminhoUsuarios'] = "usuarios.csv";
+$GLOBALS['caminhoUsuarios'] = $_SERVER['DOCUMENT_ROOT']. "/aw2-2019-stream" . "/back/model/". "usuarios.csv";
+
 $GLOBALS['adicionar'] = function($ar){
     $f = fopen($GLOBALS['caminhoUsuarios'], 'a');
     fputcsv($f, $ar);
     fclose($f);
     return true;
 };
+
 $GLOBALS['listar'] = function(){
     $f = fopen($GLOBALS['caminhoUsuarios'], 'r');
     $retorno = array();
+    $isLineOne = true;
     while (($linha = fgetcsv($f)) !== FALSE) {
-        $retorno[]=$linha;
+        if($isLineOne){
+            $isLineOne = false;
+        }else{
+            $retorno[]=$linha;
+        }
     }
     fclose($f);
     return $retorno;
 };
 
+function adicionar($id, $username, $senha, $ehAdm){
+    if(file_exists($GLOBALS['caminhoUsuarios'])){
+        $register_exists = buscarPorId($id) != null;
+        if(!$register_exists){
+            return $GLOBALS['adicionar'](array($id, $username, $senha, $ehAdm));
+        }else{
+            return false;
+        }
+    }else{
+        $f = fopen($GLOBALS['caminhoUsuarios'], 'a');
+        fclose($f);
+        $campos = array('id', 'username', 'senha', 'ehAdm');
+        $GLOBALS['adicionar']($campos);
+        return $GLOBALS['adicionar'](array($id, $username, $senha, $ehAdm));
+    }
+}
+
+function buscarPorId($id){
+    if(file_exists($GLOBALS['caminhoUsuarios'])){
+        foreach($GLOBALS['listar']() as $linha){
+            if($linha[0] == $id){
+                return array("id" => $linha[0], "username", $linha[1], "senha" => $linha[2], "isAdm" => $linha[3]);
+            }
+        }
+        return false;
+    }else{
+        return false;
+    }
+}
+
+function listarUsuarios(){
+    if(file_exists($GLOBALS['caminhoUsuarios'])){
+        return $GLOBALS['listar']();
+    }else{
+        return false;
+    }
+}
+
 
 return [
-    'adicionarUsuario' => function($id, $username, $senha, $ehAdm){
-        if(file_exists($GLOBALS['caminhoUsuarios'])){
-            return $GLOBALS['adicionar'](array($id, $username, $senha, $ehAdm));
-        }else{
-            $f = fopen($GLOBALS['caminhoUsuarios'], 'a');
-            fclose($f);
-            $campos = array('id', 'username', 'senha', 'ehAdm');
-            $GLOBALS['adicionar']($campos);
-            return $GLOBALS['adicionar'](array($id, $username, $senha, $ehAdm));
-        }
-    },
-    'listarUsuarios' => function(){
-        if(file_exists($GLOBALS['caminhoUsuarios'])){
-            return $GLOBALS['listar']();
-        }else{
-            return false;
-        }
-    },
-    'buscarUsuarioPorId' => function($id){
-        if(file_exists($GLOBALS['caminhoUsuarios'])){
-            foreach($GLOBALS['listar']() as $linha){
-                if($linha[0] == $id){
-                    echo $linha[3];
-                    return array("id" => $linha[0], "username", $linha[1], "senha" => $linha[2], "isAdm" => $linha[3]);
-                }
-            }
-            return false;
-        }else{
-            return false;
-        }
-    }
+    'adicionarUsuario' => 'adicionar',
+    'listarUsuarios' => 'listarUsuarios', 
+    'buscarUsuarioPorId' => 'buscarPorId'
 ]
 ?>
--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php  
 $userDao = require '../back/UsuariosDAO.php' ;
-echo $userDao['adicionarUsuario']('12', "joao", "12345678", 'true') .'<br>';
-echo $userDao['adicionarUsuario']('15', "joana", "12345672", 'false').'<br>';
+echo $userDao['adicionarUsuario']('12', "joao", "12345678", 'true') ? 'true':'false' .'<br>';
+echo $userDao['adicionarUsuario']('15', "joana", "12345672", 'false') ? 'true':'false'.'<br>';
 echo json_encode($userDao['listarUsuarios']()).'<br>';
 echo json_encode($userDao['buscarUsuarioPorId'](13)).'<br>';
 
