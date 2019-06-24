c0f6504eaab5d1a9da8fb904d65f660b67349e96
Victor <victorh.girardi@hotmail.com>
2019-06-23 21:51:45 -0300

    Exibição de categoria, assistir depois e favoritos

--- public/html/home.php
+++ public/html/home.php
@@ -16,10 +16,10 @@
             <div class="col-sm-12 col-md-8 col-lg-6 mx-auto">
                 <div class="d-flex justify-content-around">
                     <!--Aplicar laço de repetição php-->
-                    <button class="btn btn-primary">Trending</button>
-                    <button class="btn btn-primary">Assistir depois</button>
-                    <button class="btn btn-primary">Categorias</button>
-                    <button class="btn btn-primary">Favoritos</button>
+                    <a class="btn btn-primary" href="#videos">Vídeos</a>
+                    <a class="btn btn-primary" href="#assistirdepois">Assistir depois</a>
+                    <a class="btn btn-primary" href="#categoriasDiv">Categorias</a>
+                    <a class="btn btn-primary" href="#favoritos">Favoritos</a>
                 </div>
             </div>
         </div>
@@ -27,22 +27,47 @@
 
     <!--Sessão-->
     <div class="col-md-10 text-white mx-auto mt-5">
-        <h2>Vídeos</h2>
+        <h2 id="videos">Vídeos</h2>
         <div class="row mt-2">
             <div class="d-flex flex-wrap w-100">
-                <!--Aplicar laço de repetição php-->
 
                 <?php
-                    $videoDao = require '../../back/VideosDAO.php';
-                    foreach($videoDao['listarVideos']() as $video){
-                        echo "<div class='col-sm-6 col-md-4 col-xl-3'>
-                        <a href='video.php?nome=".$video['nome']."&link=".$video['link']."&visualizacoes=".$video['visualizacoes']."' class='card w-100 mt-3'>
+                $videoDao = require '../../back/VideosDAO.php';
+                foreach ($videoDao['listarVideos']() as $video) {
+                    echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                        <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "' class='card w-100 mt-3'>
                             <div class='card-body'>
-                                <div class='card-title font-weight-bold text-center my-auto'>".$video['nome']."</div>
+                                <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
                             </div>
                         </a>
                     </div>";
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+
+    <!--Sessão-->
+    <div class="col-md-10 text-white mx-auto mt-5">
+        <h2 id="assistirdepois">Assistir mais tarde</h2>
+        <div class="row mt-2">
+            <div class="d-flex flex-wrap w-100">
+
+                <?php
+                $assistirDao = require '../../back/AssistirMaisTardeDAO.php';
+                foreach ($assistirDao['listarAssistirMaisTarde']() as $video) {
+                    if ($video['fk_usuario'] == $_COOKIE['userId']) {
+                        echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "' class='card w-100 mt-3'>
+                                <div class='card-body'>
+                                    <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
+                                </div>
+                            </a>
+                        </div>";
+                    } else {
+                        continue;
                     }
+                }
                 ?>
 
             </div>
@@ -50,102 +75,57 @@
     </div>
 
     <!--Sessão-->
+    <?php $categoriaDao = require '../../back/model/categorias.php';
+    for($i = 1; $i <= 6; $i++){
+    ?>
     <div class="col-md-10 text-white mx-auto mt-5">
-        <h2>Assistir Depois</h2>
+        <h2><?php echo $categoriaDao['buscarCategoriaPorId']($i); ?></h2>
         <div class="row mt-2">
             <div class="d-flex flex-wrap w-100">
-                <!--Aplicar laço de repetição php-->
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
+                <?php 
+                    foreach($videoDao['videosPorCategoria']($i) as $video){
+                        echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "' class='card w-100 mt-3'>
+                                <div class='card-body'>
+                                    <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
+                                </div>
+                            </a>
+                        </div>";
+                    }
+                ?>
 
             </div>
         </div>
     </div>
 
-    <!--Sessão-->
-    <div class="col-md-10 text-white mx-auto mt-5">
-        <h2>Categorias</h2>
-        <div class="row mt-2">
-            <div class="d-flex flex-wrap w-100">
-                <!--Aplicar laço de repetição php-->
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
 
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-            </div>
-        </div>
-    </div>
+    <?php
+    }
+    ?>
+    
 
     <!--Sessão-->
     <div class="col-md-10 text-white mx-auto mt-5">
-        <h2>Favoritos</h2>
+        <h2 id="favoritos">Favoritos</h2>
         <div class="row mt-2">
             <div class="d-flex flex-wrap w-100">
-                <!--Aplicar laço de repetição php-->
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
 
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
-
-                <div class="col-sm-6 col-md-4 col-xl-3">
-                    <img class="w-100 img-fluid rounded my-2" href="" src="../img/filmes/coracaovalente.jpg">
-                </div>
+                <?php
+                $favoritosDao = require '../../back/FavoritosDAO.php';
+                foreach ($favoritosDao['listarFavoritos']() as $video) {
+                    if ($video['fk_usuario'] == $_COOKIE['userId']) {
+                        echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "' class='card w-100 mt-3'>
+                                <div class='card-body'>
+                                    <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
+                                </div>
+                            </a>
+                        </div>";
+                    } else {
+                        continue;
+                    }
+                }
+                ?>
 
             </div>
         </div>
