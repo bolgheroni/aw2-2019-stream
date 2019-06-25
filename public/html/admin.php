cdbb8c32d2c0ddb2359ab10c4719fc5b245a2fb8
Victor <victorh.girardi@hotmail.com>
2019-06-25 15:41:57 -0300

    Forms Admin e alimentação do bdd

--- public/html/admin.php
+++ public/html/admin.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<?php $categoriaDao = require '../../back/model/categorias.php';
+$videoDao = require '../../back/VideosDAO.php';
+$userDao = require '../../back/UsuariosDAO.php' ;
+?>
 <head>
 
     <meta charset="UTF-8">
@@ -15,9 +18,10 @@
     <div class="container">
         <div class="row justify-content-center align-items-center" style="height:100vh">
             <div class="col-md-6 mx-auto text-center">
+            <img width="35%" class="mt-5" src="../img/logo.png">
             <div class=" text-center mx-auto text-light">
-                <a class="btn btn-outline-primary text-white w-100" href="home.php">Voltar a home</a>
-                    <img width="35%" class="mt-5" src="../img/logo.png">
+                <a class="btn btn-outline-primary text-white w-100 mt-5" href="home.php">Voltar a home</a>
+                    
                     <h1 class="mt-4 mb-4">Cadastrar vídeo</h1>
                 </div>
                 <form id="cadastroVideo" method="post" action="admin.php">
@@ -31,17 +35,18 @@
                         <div class='form-group'>
                         <select class='form-control' name='categoria'>";
 
-                        $categoriaDao = require '../../back/model/categorias.php';
+                        
                         for ($i = 1; $i <= 6; $i++) {
                             echo "<option value=" . $i . ">" . $categoriaDao['buscarCategoriaPorId']($i) . "</option>";
                         }
-                        echo "</div>";
+                        echo "</select></div>";
                         echo "<button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar Vídeo</button>
                         </div>";
                     } else {
-                        $videoDao = require '../../back/VideosDAO.php';
+                        
+                        if(isset($_POST['nome']) && isset($_POST['link']) && isset($_POST['categoria'])){
                         if (
-                            !(is_null($_POST['nome']) || is_null($_POST['link']) || is_null($_POST['categoria']))
+                            !(is_null($_POST['nome']) && is_null($_POST['link']) && is_null($_POST['categoria']))
                             && $videoDao['adicionarVideo']($_POST['nome'], $_POST['link'], 0, $_POST['categoria'])
                         ) {
                             echo "<div class='form-group mx-auto text-center'>
@@ -51,11 +56,10 @@
                             <div class='form-group'>
                             <select class='form-control' name='categoria'>";
 
-                            $categoriaDao = require '../../back/model/categorias.php';
                             for ($i = 1; $i <= 6; $i++) {
                                 echo "<option value=" . $i . ">" . $categoriaDao['buscarCategoriaPorId']($i) . "</option>";
                             }
-                            echo "</div>";
+                            echo "</select></div>";
                             echo "<button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar Vídeo</button>
                             </div>";
 
@@ -68,17 +72,32 @@
                             <div class='form-group'>
                             <select class='form-control' name='categoria'>";
 
-                            $categoriaDao = require '../../back/model/categorias.php';
                             for ($i = 1; $i <= 6; $i++) {
                                 echo "<option value=" . $i . ">" . $categoriaDao['buscarCategoriaPorId']($i) . "</option>";
                             }
-                            echo "</div>";
+                            echo "</select></div>";
                             echo "<button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar Vídeo</button>
                             </div>";
 
-                            echo "<p class='text-danter mt-3'>Complete o formulário corretamente</p>";
+                            echo "<p class='text-danger mt-3'>Complete o formulário corretamente</p>";
                         }
+                    } else {
+                        echo "<div class='form-group mx-auto text-center'>
+                            <input type='text' class='form-control mt-5 mb-3' name='nome' placeholder='Nome do Vídeo'>
+                            <input type='text' class='form-control mb-3' name='link' placeholder='Link do vídeo'>
+    
+                            <div class='form-group'>
+                            <select class='form-control' name='categoria'>";
+    
+                            
+                            for ($i = 1; $i <= 6; $i++) {
+                                echo "<option value=" . $i . ">" . $categoriaDao['buscarCategoriaPorId']($i) . "</option>";
+                            }
+                            echo "</select></div>";
+                            echo "<button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar Vídeo</button>
+                            </div>";
                     }
+                } 
                     ?>
                 </form>
 
@@ -86,7 +105,7 @@
                 <!-- -------------------------Nomeação de Admin----------------------------- -->
                 <!-- ----------------------------------------------------------------------- -->
                 <div class=" text-center mx-auto text-light">
-                    <img width="35%" class="mt-5" src="../img/logo.png">
+                    
                     <h1 class="mt-4 mb-4">Nomeie outro Administrador</h1>
                 </div>
 
@@ -95,24 +114,25 @@
                     <?php 
                         if(!$_POST){
                             echo "<div class='form-group mx-auto text-center'>
-                            <input type='email' class='form-control mt-5 mb-3' name='user' placeholder='E-mail do novo administrador'>
+                            <input type='email' class='form-control mt-5 mb-3' name='email' placeholder='E-mail do novo administrador'>
                             <button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar</button>
                             </div>";
                         } else {
-                            $userDao = require '../../back/UsuariosDAO.php' ;
+                            
+                            if(isset($_POST['email'])){
                             if(!(is_null($_POST['email']))){
                                 if($userDao['buscarUsuarioPorEmail']($_POST['email']) != false){
                                     $usuario = $userDao['buscarUsuarioPorEmail']($_POST['email']);
-                                    $userDao['editarUsuario']($usuario['id'], $usuario['email'], $usuario['username'], $usuario['senha'], true);
+                                    $userDao['editarUsuario']($usuario['id'], $usuario['email'], $usuario['username'], $usuario['senha'], 'true');
                                     echo "<div class='form-group mx-auto text-center'>
-                                    <input type='email' class='form-control mt-5 mb-3' name='user' placeholder='E-mail do novo administrador'>
+                                    <input type='email' class='form-control mt-5 mb-3' name='email' placeholder='E-mail do novo administrador'>
                                     <button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar</button>
                                     <p class='text-primary mt-3'>O usuário indicado agora é administrador ;)</p>
                                     </div>
                                     ";
                                 } else {
                                     echo "<div class='form-group mx-auto text-center'>
-                                    <input type='email' class='form-control mt-5 mb-3' name='user' placeholder='E-mail do novo administrador'>
+                                    <input type='email' class='form-control mt-5 mb-3' name='email' placeholder='E-mail do novo administrador'>
                                     <button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar</button>
                                     <p class='text-danger mt-3'>E-mail não existe</p>
                                     </div>
@@ -121,12 +141,18 @@
                                 
                             } else {
                                 echo "<div class='form-group mx-auto text-center'>
-                                <input type='email' class='form-control mt-5 mb-3' name='user' placeholder='E-mail do novo administrador'>
+                                <input type='email' class='form-control mt-5 mb-3' name='email' placeholder='E-mail do novo administrador'>
                                 <button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar</button>
                                 <p class='text-danger mt-3'>Preencha o formulário corretamente</p>
                                 </div>
                                 ";
                             }
+                        } else {
+                            echo "<div class='form-group mx-auto text-center'>
+                            <input type='email' class='form-control mt-5 mb-3' name='email' placeholder='E-mail do novo administrador'>
+                            <button type='submit' class='text-white btn btn-outline-primary w-100'>Cadastrar</button>
+                            </div>";
+                        }
                             
                         }
                     ?>
