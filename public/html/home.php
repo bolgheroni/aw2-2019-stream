1e3f7f07cc73e40083e4bd622a63e6e78b081bff
Victor <victorh.girardi@hotmail.com>
2019-06-25 15:19:18 -0300

    Exibição das sessões na home page

--- public/html/home.php
+++ public/html/home.php
@@ -1,5 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+$usuarioDao = require '../../back/UsuariosDAO.php';
+$videoDao = require '../../back/VideosDAO.php';
+$assistirDao = require '../../back/AssistirMaisTardeDAO.php';
+$categoriaDao = require '../../back/model/categorias.php';
+$favoritosDao = require '../../back/FavoritosDAO.php';
+?>
 
 <head>
     <meta charset="UTF-8">
@@ -20,14 +27,14 @@
                     <a class="btn btn-primary" href="#assistirdepois">Assistir depois</a>
                     <a class="btn btn-primary" href="#categoriasDiv">Categorias</a>
                     <a class="btn btn-primary" href="#favoritos">Favoritos</a>
-                    <?php 
-                        $usuarioDao = require '../../back/UsuariosDAO.php';
-                        $usuario = $usuarioDao['buscarUsuarioPorId']($_COOKIE['userId']);
-                        if($usuario != false){
-                            if($usuario['ehAdm'] == true){
-                                echo "<a class='btn btn-primary' href='admin.php'>Admin</a>";  
-                            }
+                    <?php
+
+                    $usuario = $usuarioDao['buscarUsuarioPorId']($_COOKIE['userId']);
+                    if ($usuario != false) {
+                        if ($usuario['ehAdm'] == true) {
+                            echo "<a class='btn btn-primary' href='admin.php'>Admin</a>";
                         }
+                    }
                     ?>
                 </div>
             </div>
@@ -41,15 +48,18 @@
             <div class="d-flex flex-wrap w-100">
 
                 <?php
-                $videoDao = require '../../back/VideosDAO.php';
-                foreach ($videoDao['listarVideos']() as $video) {
-                    echo "<div class='col-sm-6 col-md-4 col-xl-3'>
-                        <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=".$video['id']."' class='card w-100 mt-3'>
+                if (count($videoDao['listarVideos']()) == 0) {
+                    echo "<p class='text-white ml-5'>Não há vídeos ainda</p>";
+                } else {
+                    foreach ($videoDao['listarVideos']() as $video) {
+                        echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                        <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=" . $video['id'] . "' class='card w-100 mt-3'>
                             <div class='card-body'>
                                 <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
                             </div>
                         </a>
                     </div>";
+                    }
                 }
                 ?>
             </div>
@@ -63,19 +73,30 @@
             <div class="d-flex flex-wrap w-100">
 
                 <?php
-                $assistirDao = require '../../back/AssistirMaisTardeDAO.php';
+
+                $existeAssistir = false;
+                $assitirListados = array();
+
                 foreach ($assistirDao['listarAssistirMaisTarde']() as $video) {
                     if ($video['fk_usuario'] == $_COOKIE['userId']) {
+                        $existeAssistir = true;
+                        $assistirListados[] = $video['fk_video'];
+                    }
+                }
+
+                if ($existeAssistir == true) {
+                    foreach ($assistirListados as $idVideo) {
+                        $video = $videoDao['buscarVideoPorId']($idVideo);
                         echo "<div class='col-sm-6 col-md-4 col-xl-3'>
-                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=".$video['id']."' class='card w-100 mt-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=" . $video['id'] . "' class='card w-100 mt-3'>
                                 <div class='card-body'>
                                     <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
                                 </div>
                             </a>
                         </div>";
-                    } else {
-                        continue;
                     }
+                } else {
+                    echo "<p class='text-white ml-5'>Não há vídeos para Assistir mais tarde</p>";
                 }
                 ?>
 
@@ -86,7 +107,7 @@
     <!--Sessão-->
 
     <?php
-    $categoriaDao = require '../../back/model/categorias.php';
+
     for ($i = 1; $i <= 6; $i++) {
 
         echo "<div class='col-md-10 text-white mx-auto mt-5'>
@@ -94,15 +115,19 @@
         <div class='row mt-2'>
             <div class='d-flex flex-wrap w-100'>";
 
-        $videoDao = require '../../back/VideosDAO.php';
-        foreach ($videoDao['videosPorCategoria']($i) as $video) {
-            echo "<div class='col-sm-6 col-md-4 col-xl-3'>
-                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=".$video['id']."' class='card w-100 mt-3'>
+
+        if (count($videoDao['videosPorCategoria']($i)) == 0) {
+            echo "<p class='text-white ml-5'>Não há vídeos nessa categoria ainda</p>";
+        } else {
+            foreach ($videoDao['videosPorCategoria']($i) as $video) {
+                echo "<div class='col-sm-6 col-md-4 col-xl-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=" . $video['id'] . "' class='card w-100 mt-3'>
                                 <div class='card-body'>
                                     <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
                                 </div>
                             </a>
                         </div>";
+            }
         }
 
         echo "
@@ -120,20 +145,33 @@
             <div class="d-flex flex-wrap w-100">
 
                 <?php
-                $favoritosDao = require '../../back/FavoritosDAO.php';
+
+                $existeFavoritos = false;
+                $favoritosListados = array();
+
                 foreach ($favoritosDao['listarFavoritos']() as $video) {
                     if ($video['fk_usuario'] == $_COOKIE['userId']) {
+                        $existeFavoritos = true;
+                        $favoritosListados[] = $video['fk_video'];
+                    }
+                }
+
+                if ($existeFavoritos == true) {
+                    foreach ($favoritosListados as $idVideo) {
+                        $video = $videoDao['buscarVideoPorId']($idVideo);
                         echo "<div class='col-sm-6 col-md-4 col-xl-3'>
-                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=".$video['id']."' class='card w-100 mt-3'>
+                            <a href='video.php?nome=" . $video['nome'] . "&link=" . $video['link'] . "&visualizacoes=" . $video['visualizacoes'] . "&idVideo=" . $video['id'] . "' class='card w-100 mt-3'>
                                 <div class='card-body'>
                                     <div class='card-title font-weight-bold text-center my-auto'>" . $video['nome'] . "</div>
                                 </div>
                             </a>
                         </div>";
-                    } else {
-                        continue;
                     }
+                } else {
+                    echo "<p class='text-white ml-5'>Não há vídeos para Assistir mais tarde</p>";
                 }
+
+
                 ?>
 
             </div>
