d3559cdeefa7b472e04d3b116689ad4985a9eee5
Victor <victorh.girardi@hotmail.com>
2019-06-25 18:07:22 -0300

    Visualização e acabou ;0

--- public/html/video.php
+++ public/html/video.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<?php
+    $videosDao = require '../../back/VideosDAO.php';
+    $videosDao['adicionarVisualizacao']($_GET['idVideo']); 
+?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
