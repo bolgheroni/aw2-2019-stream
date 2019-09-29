a9a0a924603c937c0fb281eb42838f5956bc777c
bolgheroni <robertobolgheroni@gmail.com>
2019-09-28 23:32:39 -0300

    Finalizacao da base das classes dao via usuariodao

--- back/DAO/UsuarioDAO.php
+++ back/DAO/UsuarioDAO.php
@@ -1,45 +1,105 @@
 <?php
-    require_once('../model/Usuario.php');
-    require_once('../Banco.php');
+    require_once($_SERVER['DOCUMENT_ROOT']. "/aw2-2019-stream/back/model/Usuario.php");
+    require_once($_SERVER['DOCUMENT_ROOT']. "/aw2-2019-stream/back/Banco.php");
 
     class UsuarioDAO{
         private $db;
         public function __construct(){
-            $this->$db = new Banco();
+            $this->db = new Banco();
         }
         
-        function adicionarUsuario($usuario){
+        function adicionarUsuario(Usuario $usuario){
             $username = $usuario->getUsername();
             $email = $usuario->getEmail();
             $senha = $usuario->getSenha();
             $ehAdm = $usuario->getEhAdm();
-
-            $query = "INSERT INTO Usuario (Username, email, senha, ehAdm) VALUES (?,?,?,?)";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_bind_param($stmt,'sssi', $username, $email, $senha, $ehAdm);
-            mysqli_stmt_execute($stmt); 
-            mysqli_stmt_close($stmt);
+            $this->db->conect();
+            $cmd = "INSERT INTO usuario(username, email, senha, ehAdm) ".
+            "VALUES('".$username."', '".$email."', '".$senha."', '".$ehAdm."')";
+            $result = mysqli_query($this->db->getConection(), $cmd);
+            if ($result == true) {
+                echo"Inser��o com sucesso!";
+                $this->db->disconect();
+                return true;
+            } else {
+                $err = mysqli_error($this->db->getConection());
+                echo "Falha na inser��o! Erro: .".$err;
+                $this->db->disconect();
+                return false;
+            }
+            // $query = "INSERT INTO usuario (username, email, senha, ehAdm) VALUES (?,?,?,?)";
+            // $stmt = mysqli_prepare($this->db->getConection(), $query);
+            // echo 'a'.$stmt.'b';
+            // mysqli_stmt_bind_param($stmt,'sssi', $username, $email, $senha, $ehAdm);
+            // mysqli_stmt_execute($stmt); 
+            // mysqli_stmt_close($stmt);
+            
         }
 
         function listarUsuarios(){
-            $query = "SELECT * FROM Usuario";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt,$usuarios);
-            mysqli_stmt_fecth($stmt);
-
-            return json_encode($usuarios);
+            $this->db->conect();
+            $cmd = "SELECT * from usuario;";
+            $query = mysqli_query($this->db->getConection(), $cmd);
+            $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+            if ($query) {
+                $this->db->disconect();
+                return $result;
+            } else {
+                $err = mysqli_error($$this->db->getConection());
+                $this->db->disconect();
+                return $err;
+            }
+            //
+            // $query = "SELECT * FROM usuario";
+            // $stmt = mysqli_prepare($this->db->getConection(), $query);
+            // mysqli_stmt_execute($stmt);
+            // mysqli_stmt_bind_result($stmt,$usuarios );
+            // mysqli_stmt_fetch_array($stmt);
         }
 
         function buscarUsuarioPorId($id){
-            $query = "SELECT * FROM Usuario WHERE id=?";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_bind_param($stmt,"s",$id);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt,$usuario);
-            mysqli_stmt_fecth($stmt);
+            $this->db->conect();
+            $cmd = "SELECT * from usuario WHERE id=".$id.";";
+            $query = mysqli_query($this->db->getConection(), $cmd);
+            $result = mysqli_fetch_assoc($query);
+            if ($query) {
+                $this->db->disconect();
+                return $result;
+            } else {
+                $err = mysqli_error($$this->db->getConection());
+                $this->db->disconect();
+                return $err;
+            }
+            // $query = "SELECT * FROM usuario WHERE id=?";
+            // $stmt = mysqli_prepare($this->$db->getConection(), $query);
+            // mysqli_stmt_bind_param($stmt,"s",$id);
+            // mysqli_stmt_execute($stmt);
+            // mysqli_stmt_bind_result($stmt,$usuario);
+            // mysqli_stmt_fecth($stmt);
 
-            return json_encode($usuario);
+            // return json_encode($usuario);
+        }
+        function buscarUsuarioPorEmail($email){
+            $this->db->conect();
+            $cmd = "SELECT * from usuario WHERE email=" . "'" .$email. "'" . ";";
+            $query = mysqli_query($this->db->getConection(), $cmd);
+            $result = mysqli_fetch_assoc($query);
+            if ($query) {
+                $this->db->disconect();
+                return $result;
+            } else {
+                $err = mysqli_error($this->db->getConection());
+                $this->db->disconect();
+                return $err;
+            }
+            // $query = "SELECT * FROM usuario WHERE email=?";
+            // $stmt = mysqli_prepare($this->$db->getConection(), $query);
+            // mysqli_stmt_bind_param($stmt,"s",$email);
+            // mysqli_stmt_execute($stmt);
+            // mysqli_stmt_bind_result($stmt,$usuario);
+            // mysqli_stmt_fecth($stmt);
+
+            // return json_encode($usuario);
         }
 
         function editarUsuario($usuario){
@@ -48,32 +108,49 @@
             $email = $usuario->getEmail();
             $senha = $usuario->getSenha();
             $ehAdm = $usuario->getEhAdm();
-
-            $query = "UPDATE Usuario SET username =?, email=?, senha=?, ehAdm=? WHERE id=?";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_bind_param($stmt,'sssis', $username, $email, $senha, $ehAdm, $id);
-            mysqli_stmt_execute($stmt); 
-            mysqli_stmt_close($stmt);
+            $this->db->conect();
+            $cmd = 'UPDATE usuario SET username=\''.$username.'\', email=\''.$email.'\', senha=\''.$senha.'\', ehAdm='.$ehAdm.
+                ' WHERE id='.$id.';';
+            $result = mysqli_query($this->db->getConection(), $cmd);
+            if ($result == true) {
+                echo"Atualizado com sucesso!";
+                $this->db->disconect();
+                return true;
+            } else {
+                $err = mysqli_error($this->db->getConection());
+                echo "Falha na atualizacao! Erro: .".$err;
+                $this->db->disconect();
+                return false;
+            }
+            // $query = "UPDATE usuario SET username =?, email=?, senha=?, ehAdm=? WHERE id=?";
+            // $stmt = mysqli_prepare($this->$db->getConection(), $query);
+            // mysqli_stmt_bind_param($stmt,'sssis', $username, $email, $senha, $ehAdm, $id);
+            // mysqli_stmt_execute($stmt); 
+            // mysqli_stmt_close($stmt);
         }
 
         function removerUsuario($id){
-            $query = "DELETE FROM Usuario WHERE id =?";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_bind_param($stmt,'s', $id);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_fecth($stmt);
+            $this->db->conect();
+            $cmd = 'DELETE from usuario WHERE id='.$id.';';
+            $result = mysqli_query($this->db->getConection(), $cmd);
+            if ($result == true) {
+                echo"Removido com sucesso!";
+                $this->db->disconect();
+                return true;
+            } else {
+                $err = mysqli_error($this->db->getConection());
+                echo "Falha na remocao! Erro: .".$err;
+                $this->db->disconect();
+                return false;
+            }
+            // $query = "DELETE FROM usuario WHERE id =?";
+            // $stmt = mysqli_prepare($this->$db->getConection(), $query);
+            // mysqli_stmt_bind_param($stmt,'s', $id);
+            // mysqli_stmt_execute($stmt);
+            // mysqli_stmt_fecth($stmt);
         }
 
-        function buscarUsuarioPorEmail($email){
-            $query = "SELECT * FROM Usuario WHERE email=?";
-            $stmt = mysqli_prepare($this->db->getConection(), $query);
-            mysqli_stmt_bind_param($stmt,"s",$email);
-            mysqli_stmt_execute($stmt);
-            mysqli_stmt_bind_result($stmt,$usuario);
-            mysqli_stmt_fecth($stmt);
-
-            return json_encode($usuario);
-        }
+        
 
 
         function autenticarUsuario(){
